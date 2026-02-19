364ccff959e4c1efd2c221ced92dc41a3c5d61e9
rania-jb <rnya001@gmail.com>
2026-02-19 13:42:46 +0100

    Ajout du build frontend React pour déploiement

---
+++ client/build/build/precache-manifest.0a4b30bcf79279ad724b7a9c1e95e883.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daa8e3cc17e747540ab014986e906bfb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6197940e800448fc2d44",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "401e745065819688c86e",
+    "url": "/static/js/2.e7b1de21.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.e7b1de21.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6197940e800448fc2d44",
+    "url": "/static/js/main.afd0e5f2.chunk.js"
+  },
+  {
+    "revision": "4387b907a4d64303b5a2",
+    "url": "/static/js/runtime-main.fab42d54.js"
+  }
+]);
---
+++ client/build/precache-manifest.1081e365a9590b48e8cfbe2eab23ee28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f6ae469c2723e204b4740943c3216a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "540063ddd77f102fefc0",
+    "url": "/static/css/main.3000fdb4.chunk.css"
+  },
+  {
+    "revision": "1599d465112c9791fffe",
+    "url": "/static/js/2.f9a14f7b.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.f9a14f7b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "540063ddd77f102fefc0",
+    "url": "/static/js/main.aa8e3704.chunk.js"
+  },
+  {
+    "revision": "4387b907a4d64303b5a2",
+    "url": "/static/js/runtime-main.fab42d54.js"
+  }
+]);
